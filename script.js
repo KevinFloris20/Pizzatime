65fdeca9aec92bb7501690c65603b10d2de91e64
KevinFloris20 <75303163+KevinFloris20@users.noreply.github.com>
2021-02-17 00:52:35 -0500

    added functions for my method calls

--- script.js
+++ script.js
@@ -1,10 +1,46 @@
+// // //./dominos/dominos-pizza-api.js
+// requirejs.config({
+//     //By default load any module IDs from js/lib
+//     baseUrl: './dominos/dominos-pizza-api.js',
+    
+// });
 
-var pizzapi = require('dominos');
+//  var myStore = new pizzapi.Store({ ID: 4336 });
 
-pizzapi.Util.findNearbyStores(
-    '07054',
-    'Delivery',
-    function(storeData){
-        console.log(storeData.result.Stores);
-    }
-);
+// var pizzapi = require('dominos');
+// var idd;
+// pizzapi.Util.findNearbyStores(
+//     '07054',
+//     'Delivery',
+//     function(storeData){
+//         console.log(storeData.result.Stores[0].StoreID);
+//         idd = storeData.result.Stores[0].StoreID;
+//     }
+// );
+var idd;
+
+function getid(){
+    var pizzapi = require('dominos');
+    pizzapi.Util.findNearbyStores(
+        '07054',
+        'Delivery',
+        function(storeData){
+            console.log(storeData.result.Stores[0].StoreID);
+            idd = storeData.result.Stores[0].StoreID;
+        }
+    );
+}
+
+function menu(){
+  var pizzapi = require('dominos');
+  var myStore = new pizzapi.Store({ ID: 4571 });
+
+  myStore.getMenu(
+      function(storeData){
+          console.log(storeData);
+      }
+  );
+}
+
+//getid();
+menu();
